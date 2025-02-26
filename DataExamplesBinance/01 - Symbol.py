9692c6e3e0f987abf040d7a15277fc1d731499ec
Fernando Furtado <fppfurtado@outlook.com>
2025-02-26 12:59:03 -0300

    ajusta utilização da classe Config no script para se adequar às últimas alterações

--- DataExamplesBinance/01 - Symbol.py
+++ DataExamplesBinance/01 - Symbol.py
@@ -11,9 +11,10 @@ if __name__ == '__main__':  # Entry point when running this script
     coin_target = 'USDT'  # the base ticker in which calculations will be performed
     symbol = 'BTC' + coin_target  # the ticker by which we will receive data in the format <CodeTickerBaseTicker>
 
+    config = Config()
     store = BinanceStore(
-        api_key=Config.BINANCE_API_KEY,
-        api_secret=Config.BINANCE_API_SECRET,
+        api_key=config.BINANCE_API_KEY,
+        api_secret=config.BINANCE_API_SECRET,
         coin_target=coin_target,
         testnet=False,
         # tld="us",  # for US customers => to use the 'Binance.us' url
